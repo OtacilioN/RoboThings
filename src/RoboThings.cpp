37af4231db2f13beaa2d17cb159cd538bb4d684a
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2017-02-20 19:22:00 +0000

    feat: add RoboThings.h and RoboThings.cpp
    Add the main base of the library

---
+++ src/RoboThings.cpp
@@ -0,0 +1,13 @@
+/*
+ * Ultrasonic.h
+ * 
+ * Library for HC-SR04 Ultrasonic Ranging Module in a minimalist way
+ *
+ * created 20 Feb 2016
+ * by Otacilio Neto <contato@otacilio.me> (github: @OtacioN), Erick Simões <erick.simoes@live.com> (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * 
+ * Released into the MIT License.
+ */
+ 
+#include <Arduino.h>
+#include "RoboThings.h"
