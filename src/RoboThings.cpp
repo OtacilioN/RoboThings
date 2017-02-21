96b610a307abd5c23ed168af466437e886476632
Otacilio Saraiva Maia Neto <otacilio.n97@gmail.com>
2017-02-21 15:27:52 +0000

    feat: add motor controll with l293d

--- src/RoboThings.cpp
+++ src/RoboThings.cpp
@@ -11,3 +11,198 @@
  
 #include <Arduino.h>
 #include "RoboThings.h"
+
+Robot::Robot(int enable, int input1, int input2, int enable2, int input3, int input4){
+    
+    pinMode(enable, OUTPUT);
+    pinMode(input1, OUTPUT);
+    pinMode(input2, OUTPUT);
+    
+    pinMode(enable2, OUTPUT);
+    pinMode(input3, OUTPUT);
+    pinMode(input4, OUTPUT);
+    
+    _enable = enable;
+    _enable2 = enable2;
+    _input1 = input1;
+    _input2 = input2;
+    _input3 = input3;
+    _input4 = input4;
+    
+}
+
+
+// Fowards movements
+void Robot::foward(int power, int ms){
+    
+    analogWrite(_enable, power);
+    analogWrite(_enable2, power);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, HIGH);
+    digitalWrite(_input4, LOW);
+    
+    delay(ms);
+}
+
+void Robot::foward(int power){
+    
+    analogWrite(_enable, power);
+    analogWrite(_enable2, power);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, HIGH);
+    digitalWrite(_input4, LOW);
+}
+
+// Backwards movements
+void Robot::backward(int power, int ms){
+    
+    analogWrite(_enable, power);
+    analogWrite(_enable2, power);
+    
+    digitalWrite(_input2, HIGH);
+    digitalWrite(_input1, LOW);
+    
+    digitalWrite(_input4, HIGH);
+    digitalWrite(_input3, LOW);
+    
+    delay(ms);
+}
+
+void Robot::backward(int power){
+    
+    analogWrite(_enable, power);
+    analogWrite(_enable2, power);
+    
+    digitalWrite(_input2, HIGH);
+    digitalWrite(_input1, LOW);
+    
+    digitalWrite(_input4, HIGH);
+    digitalWrite(_input3, LOW);
+}
+
+// Left Movements
+void Robot::left(int power, int ms){
+    
+    analogWrite(_enable, power);
+    analogWrite(_enable2, 0);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, LOW);
+    digitalWrite(_input4, LOW);
+    
+    delay(ms);
+}
+
+void Robot::left(int power){
+    
+    analogWrite(_enable, power);
+    analogWrite(_enable2, 0);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, LOW);
+    digitalWrite(_input4, LOW);
+}
+
+// Right Movements
+void Robot::Right(int power, int ms){
+    
+    analogWrite(_enable, 0);
+    analogWrite(_enable2, power);
+    
+    digitalWrite(_input1, LOW);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, HIGH);
+    digitalWrite(_input4, LOW);
+    
+    delay(ms);
+}
+
+void Robot::Right(int power){
+    
+    analogWrite(_enable, 0);
+    analogWrite(_enable2, power);
+    
+    digitalWrite(_input1, LOW);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, HIGH);
+    digitalWrite(_input4, LOW);
+}
+
+// Stop Movements
+void Robot::stop(){
+    analogWrite(_enable, 0);
+    analogWrite(_enable2, 0);
+    
+    digitalWrite(_input1, LOW);
+    digitalWrite(_input2, LOW);
+    
+    digitalWrite(_input3, LOW);
+    digitalWrite(_input4, LOW);
+}
+
+
+// Controlling a singular motor
+Motor::Motor(int enable, int input1, int input2){
+    
+    pinMode(enable, OUTPUT);
+    pinMode(input1, OUTPUT);
+    pinMode(input2, OUTPUT);
+    
+    _enable = enable;
+    
+    _input1 = input1;
+    _input2 = input2;
+    
+}
+
+void Motor::clockwise(int power, int ms){
+    analogWrite(_enable, power);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+    delay(ms);
+}
+
+void Motor::clockwise(int power){
+    analogWrite(_enable, power);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+}
+
+void Motor::clockwise(int power, int ms){
+    analogWrite(_enable, power);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+    delay(ms);
+}
+
+void Motor::clockwise(int power){
+    analogWrite(_enable, power);
+    
+    digitalWrite(_input1, HIGH);
+    digitalWrite(_input2, LOW);
+    
+}
+
+void Motor::stop(){
+    analogWrite(_enable, 0);
+    
+    digitalWrite(_input1, LOW);
+    digitalWrite(_input2, LOW);
+}
--- src/RoboThings.h
+++ src/RoboThings.h
@@ -14,5 +14,59 @@
 
 #include "Ultrasonic.h"
 
+// Class to use a 2WD Robot using a DC Motor and a L293D H bridge
+class Robot{
+    
+    public:
+    
+        Robot(int enable, int input1, int input2, int enable2, int input3, int input4);
+    
+        void foward(int power, int ms);
+        void foward(int power);
+    
+        void backward(int power, int ms);
+        void backward(int power);
+    
+        void left(int power, int ms);
+        void left(int power);
+    
+        void right(int power, int ms);
+        void right(int power);
+        
+        void stop();
+    
+    private:
+        int _enable;
+        int _enable2;
+        int _input1;
+        int _input2;
+        int _input3;
+        int _input4;
+    
+};
+
+class Motor{
+    
+    public:
+    
+        Motor(int enable, int input1, int input2);
+        
+        void clockwise(int power, int ms);
+        void clockwise(int power);
+    
+        void counterclockwise(int power, int ms);
+        void counterclockwise(int power);
+        
+        void stop();
+    
+    private:
+        
+        int _enable;
+        int _input1;
+        int _input2;
+};
+
+
+
 
 #endif
